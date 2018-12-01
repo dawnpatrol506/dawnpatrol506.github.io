65cd64f05a4180da9bd556f23a0a8cc91c42d572
Kevin Davis <dawnpatrol506@gmail.com>
2018-11-30 21:58:22 -0700

    done for now

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,29 @@
-$(document).ready(function(){
-  $('.tabs').tabs();
-  
-  $('.tabs').forEach(tab => {
-      tab.addClass('white-text');
-  })
+$(document).ready(function () {
+    $('.tabs').tabs();
+    $('.collapsible').collapsible();
+
+    $('.tab').on('click', function (e) {
+        let tab = $(this).attr('data-name');
+        let about = $('#about');
+        let portfolio = $('#portfolio');
+        let contact = $('#contact');
+
+        switch (tab) {
+            case 'about':
+                portfolio.hide();
+                contact.hide();
+                about.show();
+                break;
+            case 'portfolio':
+                contact.hide();
+                about.hide();
+                portfolio.show();
+                break;
+            case 'contact':
+                about.hide();
+                portfolio.hide();
+                contact.show();
+                break;
+        }
+    })
 })
